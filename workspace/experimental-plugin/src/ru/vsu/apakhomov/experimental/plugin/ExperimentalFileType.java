498265348a53cf6417c90114bc7f1d35f3f63a8d
apakhomov <apakhomov@gridfore.com>
2019-03-10 14:23:03 +0300

    Create simple idea plugin with icon support

---
+++ workspace/experimental-plugin/src/ru/vsu/apakhomov/experimental/plugin/ExperimentalFileType.java
@@ -0,0 +1,39 @@
+package ru.vsu.apakhomov.experimental.plugin;
+
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class ExperimentalFileType extends LanguageFileType {
+    public static final ExperimentalFileType INSTANCE = new ExperimentalFileType();
+
+    private ExperimentalFileType() {
+        super(ExperimentalLang.INSTANCE);
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return "Experimental file";
+    }
+
+    @NotNull
+    @Override
+    public String getDescription() {
+        return "Experimental language file";
+    }
+
+    @NotNull
+    @Override
+    public String getDefaultExtension() {
+        return "exp";
+    }
+
+    @Nullable
+    @Override
+    public Icon getIcon() {
+        return ExperimentalIcons.FILE;
+    }
+}
---
+++ workspace/experimental-plugin/src/ru/vsu/apakhomov/experimental/plugin/ExperimentalIcons.java
@@ -0,0 +1,9 @@
+package ru.vsu.apakhomov.experimental.plugin;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class ExperimentalIcons {
+    public static final Icon FILE = IconLoader.getIcon("/ru/vsu/apakhomov/experimental/plugin/icons/icon2.png");
+}
