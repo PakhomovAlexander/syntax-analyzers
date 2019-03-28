b8cf83c9da3edf06b22aa5732487cb26798ca5dc
apakhomov <apakhomov@gridfore.com>
2019-03-28 22:14:39 +0300

    Add Reference, Refactoring and Completion support to experimental plugin

--- workspace/experimental-plugin/src/ru/vsu/apakhomov/experimental/plugin/psi/impl/ExperimentalPsiImplUtil.java
+++ workspace/experimental-plugin/src/ru/vsu/apakhomov/experimental/plugin/psi/impl/ExperimentalPsiImplUtil.java
@@ -1,6 +1,8 @@
 package ru.vsu.apakhomov.experimental.plugin.psi.impl;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import ru.vsu.apakhomov.experimental.plugin.psi.ExperimentalElementFactory;
 import ru.vsu.apakhomov.experimental.plugin.psi.ExperimentalProperty;
 import ru.vsu.apakhomov.experimental.plugin.psi.ExperimentalTypes;
 
@@ -19,6 +21,30 @@ public class ExperimentalPsiImplUtil {
         ASTNode valueNode = element.getNode().findChildByType(ExperimentalTypes.VALUE);
         if (valueNode != null) {
             return valueNode.getText();
+        } else {
+            return null;
+        }
+    }
+
+    public static String getName(ExperimentalProperty element) {
+        return getKey(element);
+    }
+
+    public static PsiElement setName(ExperimentalProperty element, String newName) {
+        ASTNode keyNode = element.getNode().findChildByType(ExperimentalTypes.KEY);
+        if (keyNode != null) {
+
+            ExperimentalProperty property = ExperimentalElementFactory.createProperty(element.getProject(), newName);
+            ASTNode newKeyNode = property.getFirstChild().getNode();
+            element.getNode().replaceChild(keyNode, newKeyNode);
+        }
+        return element;
+    }
+
+    public static PsiElement getNameIdentifier(ExperimentalProperty element) {
+        ASTNode keyNode = element.getNode().findChildByType(ExperimentalTypes.KEY);
+        if (keyNode != null) {
+            return keyNode.getPsi();
         } else {
             return null;
         }
