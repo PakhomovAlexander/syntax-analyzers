811fed70f559b7cee8570f60b365ea80d3fa3e89
apakhomov <apakhomov@gridfore.com>
2019-03-29 13:28:54 +0300

    Add Commenter to plugin

---
+++ workspace/experimental-plugin/src/ru/vsu/apakhomov/experimental/plugin/ExperimentalCommenter.java
@@ -0,0 +1,36 @@
+package ru.vsu.apakhomov.experimental.plugin;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+public class ExperimentalCommenter implements Commenter {
+    @Nullable
+    @Override
+    public String getLineCommentPrefix() {
+        return "#";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentPrefix() {
+        return "";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentSuffix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentPrefix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+}
