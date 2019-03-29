87dc837af5326a39141db844daa50139377c1906
apakhomov <apakhomov@gridfore.com>
2019-03-29 13:25:16 +0300

    Add CodeStyleSettings to plugin

---
+++ workspace/experimental-plugin/src/ru/vsu/apakhomov/experimental/plugin/ExperimentalCodeStyleSettings.java
@@ -0,0 +1,9 @@
+package ru.vsu.apakhomov.experimental.plugin;
+
+import com.intellij.psi.codeStyle.*;
+
+public class ExperimentalCodeStyleSettings extends CustomCodeStyleSettings {
+    public ExperimentalCodeStyleSettings(CodeStyleSettings settings) {
+        super("ExperimentalCodeStyleSettings", settings);
+    }
+}
